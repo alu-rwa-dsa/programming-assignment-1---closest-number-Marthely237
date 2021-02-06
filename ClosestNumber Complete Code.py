500fc582410862bba72cedac8fe406d6119b5d93
Marthely237 <n.marthely@alustudent.com>
2021-02-06 15:07:48 +0200

    Assignment 1

---
+++ ClosestNumber Complete Code.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the closestNumbers function below.
+def closestNumbers(arr):
+
+    arr.sort()
+    min_dif = abs(arr[0]-arr[1])
+    ans = []
+    for i in range(len(arr)-1):
+        d = abs(arr[i]-arr[i+1])
+        if d==min_dif:
+            ans += [arr[i], arr[i+1]]
+            min_dif =d
+        elif d<min_dif:
+            ans = [arr[i], arr[i+1]]
+            min_dif =d
+    return ans
+
+input()
+print(*closestNumbers(list(map(int,input().split()))))
+
